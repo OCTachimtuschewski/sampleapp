ee5d6b5da656d1bba0681687237f364b168301a4
OCTachimtuschewski <achim.tuschewski@octanner.com>
2013-02-25 11:58:15 -0700

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -76,66 +76,83 @@ describe "Authentication" do
           end
 
           describe "when signing in again" do
-              before do
-                visit signin_path
-                fill_in "Email", with: user.email
-                fill_in "Password", with: user.password
-                click_button "Sign in"
-              end
-              it "should render the default (profile) page" do
-                page.should have_selector('title', text: user.name)
-              end
+            before do
+              visit signin_path
+              fill_in "Email", with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name)
             end
           end
         end
       end
 
+      describe "in the Microposts controller" do
 
-      describe "in the Users controller" do
-
-        describe "visiting the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_selector('title', text: 'Sign in') }
-        end
-
-        describe "submitting to the update action" do
-          before { put user_path(user) }
+        describe "submitting to the create action" do
+          before { post microposts_path }
           specify { response.should redirect_to(signin_path) }
         end
 
-        describe "visiting the user index" do
-          before { visit users_path }
-          it { should have_selector('title', text: 'Sign in') }
+        describe "submitting to the destroy action" do
+          before do
+            micropost = FactoryGirl.create(:micropost)
+            delete micropost_path(micropost)
+          end
+          specify { response.should redirect_to(signin_path) }
         end
       end
     end
 
-    describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user }
 
-      describe "visiting Users#edit page" do
-        before { visit edit_user_path(wrong_user) }
-        it { should_not have_selector('title', text: full_title('Edit user')) }
+    describe "in the Users controller" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "visiting the edit page" do
+        before { visit edit_user_path(user) }
+        it { should have_selector('title', text: 'Sign in') }
       end
 
-      describe "submitting a PUT request to the Users#update action" do
-        before { put user_path(wrong_user) }
-        specify { response.should redirect_to(root_path) }
+      describe "submitting to the update action" do
+        before { put user_path(user) }
+        specify { response.should redirect_to(signin_path) }
       end
-    end
 
-    describe "as non-admin user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:non_admin) { FactoryGirl.create(:user) }
-
-      before { sign_in non_admin }
-
-      describe "submitting a DELETE request to the Users#destroy action" do
-        before { delete user_path(user) }
-        specify { response.should redirect_to(root_path) }
+      describe "visiting the user index" do
+        before { visit users_path }
+        it { should have_selector('title', text: 'Sign in') }
       end
     end
   end
+
+  describe "as wrong user" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+    before { sign_in user }
+
+    describe "visiting Users#edit page" do
+      before { visit edit_user_path(wrong_user) }
+      it { should_not have_selector('title', text: full_title('Edit user')) }
+    end
+
+    describe "submitting a PUT request to the Users#update action" do
+      before { put user_path(wrong_user) }
+      specify { response.should redirect_to(root_path) }
+    end
+  end
+
+  describe "as non-admin user" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:non_admin) { FactoryGirl.create(:user) }
+
+    before { sign_in non_admin }
+
+    describe "submitting a DELETE request to the Users#destroy action" do
+      before { delete user_path(user) }
+      specify { response.should redirect_to(root_path) }
+    end
+  end
 end
+
